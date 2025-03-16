a9f1e2a4c02c1a594db44e2cb2ffca6b10481760
Luiz Alves <Luizzapn@gmail.com>
2025-03-16 10:46:27 -0300

    Aula e prática

--- tipo-variaveis/src/TiposVariaveis.java
+++ tipo-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,12 @@
-public class App {
+public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        String nome = "Neto";
+        int idade = 31;
+        double peso = 100.50;
+        float pi = 3.14F;
+        long cpf = 12345678999L;
+        double salario = 2100.90;
+        boolean doador = true;
+        char sexo = 'M';
     }
 }
